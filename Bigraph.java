5b193f2ac4dbbb692cb97603b97321299da6f69b
arkainoh <arkainoh@gmail.com>
2017-04-16 10:08:56 +0900

    add Bigraph

---
+++ Bigraph.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Bigraph {
+	private int V, E;
+	private ArrayList<Integer>[] adj;
+	
+	public Bigraph(int V) {
+		this.V = V;
+		adj = new ArrayList[V];
+		for(int i = 0; i < V; i++)
+			adj[i] = new ArrayList<Integer>();
+	}
+	
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+	
+	public void addEdge(int u, int v) {
+		E++;
+		adj[u].add(v);
+		adj[v].add(u);
+	}
+
+	public int V() {
+		return V;
+	}
+
+	public int E() {
+		return E;
+	}
+
+	@Override
+	public String toString() {
+		return "Bigraph [" + Arrays.toString(adj) + "]";
+	}
+}
