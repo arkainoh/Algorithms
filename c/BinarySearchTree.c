c9a5b7665814216ab65d1caffd4828f5a3f8e572
arkainoh <arkainoh@gmail.com>
2018-05-27 12:16:32 +0900

    modify indentation

--- c/BinarySearchTree.c
+++ c/BinarySearchTree.c
@@ -3,21 +3,21 @@
 
 typedef struct _treeNode* TreeNode;
 typedef struct _treeNode {
-	int key, val;
-	TreeNode left, right, parent;
+  int key, val;
+  TreeNode left, right, parent;
 } treeNode;
 
 typedef struct _binarySearchTree {
-	TreeNode root;
-	int size;
+  TreeNode root;
+  int size;
 } binarySearchTree;
 typedef binarySearchTree* BinarySearchTree;
 
 BinarySearchTree newBinarySearchTree() {
-	BinarySearchTree ret = (BinarySearchTree)malloc(sizeof(binarySearchTree));
-	ret->root = NULL;
-	ret->size = 0;
-	return ret;
+  BinarySearchTree ret = (BinarySearchTree)malloc(sizeof(binarySearchTree));
+  ret->root = NULL;
+  ret->size = 0;
+  return ret;
 }
 
 TreeNode newTreeNode(int key, int val) {
@@ -29,180 +29,180 @@ TreeNode newTreeNode(int key, int val) {
 }
 
 TreeNode _put(BinarySearchTree bst, TreeNode root, int key, int val) {
-	if(!root) {
-		bst->size++;
-		return newTreeNode(key, val);
-	}
+  if(!root) {
+    bst->size++;
+    return newTreeNode(key, val);
+  }
 
-	if(key < root->key) {
-		TreeNode l = _put(bst, root->left, key, val);
-		if(l) l->parent = root;
-		root->left = l;
-	}
-	else if(key > root->key) {
-		TreeNode r = _put(bst, root->right, key, val);
-		if(r) r->parent = root;
-		root->right = r;
-	}
-	else root->val = val;
-	return root;
+  if(key < root->key) {
+    TreeNode l = _put(bst, root->left, key, val);
+    if(l) l->parent = root;
+    root->left = l;
+  }
+  else if(key > root->key) {
+    TreeNode r = _put(bst, root->right, key, val);
+    if(r) r->parent = root;
+    root->right = r;
+  }
+  else root->val = val;
+  return root;
 }
 
 void put(BinarySearchTree bst, int key, int val) {
-	bst->root = _put(bst, bst->root, key, val);
+  bst->root = _put(bst, bst->root, key, val);
 }
 
 TreeNode rmv(BinarySearchTree bst, int key) {
-	TreeNode iter = bst->root, tmp;
-	while(iter) {
-		if(key < iter->key) iter = iter->left;
-		else if(key > iter->key) iter = iter->right;
-		else {
-			bst->size--;
-			if(!iter->left && !iter->right) tmp = NULL;
-			else if(iter->left && !iter->right) tmp = iter->left;
-			else if(!iter->left && iter->right) tmp = iter->right;
-			else {
-				TreeNode iter2 = iter->right;
-				while(iter2->left) iter2 = iter2->left;
-				if(iter2->parent->left->key == iter2->key) iter2->parent->left = iter2->right;
-				if(iter2->parent->right->key == iter2->key) iter2->parent->right = iter2->right;
-				if(iter2->right) iter2->right->parent = iter2->parent;
+  TreeNode iter = bst->root, tmp;
+  while(iter) {
+    if(key < iter->key) iter = iter->left;
+    else if(key > iter->key) iter = iter->right;
+    else {
+      bst->size--;
+      if(!iter->left && !iter->right) tmp = NULL;
+      else if(iter->left && !iter->right) tmp = iter->left;
+      else if(!iter->left && iter->right) tmp = iter->right;
+      else {
+        TreeNode iter2 = iter->right;
+        while(iter2->left) iter2 = iter2->left;
+        if(iter2->parent->left->key == iter2->key) iter2->parent->left = iter2->right;
+        if(iter2->parent->right->key == iter2->key) iter2->parent->right = iter2->right;
+        if(iter2->right) iter2->right->parent = iter2->parent;
 
-				iter2->left = iter->left;
-				iter2->right = iter->right;
+        iter2->left = iter->left;
+        iter2->right = iter->right;
 
-				iter->left->parent = iter2;
-				if(iter->right) iter->right->parent = iter2;
-				tmp = iter2;
-			}
-			
-			if(!iter->parent) bst->root = tmp;
-			else {
-				if(iter->parent->left && iter->parent->left->key == iter->key) iter->parent->left = tmp;
-				if(iter->parent->right && iter->parent->right->key == iter->key) iter->parent->right = tmp;
-			}
+        iter->left->parent = iter2;
+        if(iter->right) iter->right->parent = iter2;
+        tmp = iter2;
+      }
+      
+      if(!iter->parent) bst->root = tmp;
+      else {
+        if(iter->parent->left && iter->parent->left->key == iter->key) iter->parent->left = tmp;
+        if(iter->parent->right && iter->parent->right->key == iter->key) iter->parent->right = tmp;
+      }
 
-			if(tmp) tmp->parent = iter->parent;
-			return iter;
-		}
-	}
-	return NULL;
+      if(tmp) tmp->parent = iter->parent;
+      return iter;
+    }
+  }
+  return NULL;
 }
 
 TreeNode search(BinarySearchTree bst, int key) {
-	TreeNode iter = bst->root;
-	while(iter) {
-		if(key < iter->key) iter = iter->left;
-		else if(key > iter->key) iter = iter->right;
-		else return iter;
-	}
-	return NULL;
+  TreeNode iter = bst->root;
+  while(iter) {
+    if(key < iter->key) iter = iter->left;
+    else if(key > iter->key) iter = iter->right;
+    else return iter;
+  }
+  return NULL;
 }
 
 void removeTreeNode(TreeNode root) {
-	if(!root) return;
-	removeTreeNode(root->left);
-	removeTreeNode(root->right);
-	free(root);
+  if(!root) return;
+  removeTreeNode(root->left);
+  removeTreeNode(root->right);
+  free(root);
 }
 
 TreeNode getMin(BinarySearchTree bst) {
-	if(!bst->root) return NULL;
-	TreeNode iter = bst->root;
-	while(iter->left) iter = iter->left;
-	return iter;
+  if(!bst->root) return NULL;
+  TreeNode iter = bst->root;
+  while(iter->left) iter = iter->left;
+  return iter;
 }
 
 TreeNode getMax(BinarySearchTree bst) {
-	if(!bst->root) return NULL;
-	TreeNode iter = bst->root;
-	while(iter->right) iter = iter->right;
-	return iter;
+  if(!bst->root) return NULL;
+  TreeNode iter = bst->root;
+  while(iter->right) iter = iter->right;
+  return iter;
 }
 
 TreeNode _floor(TreeNode root, int key) {
-	TreeNode iter = root, tmp = NULL;
-	while(iter) {
-		if(key < iter->key) iter = iter->left;
-		else if(key > iter->key) {
-			tmp = _floor(root->right, key);
-			if(tmp) return tmp;
-			break;
-		} else return iter;
-	}
-	return iter;
+  TreeNode iter = root, tmp = NULL;
+  while(iter) {
+    if(key < iter->key) iter = iter->left;
+    else if(key > iter->key) {
+      tmp = _floor(root->right, key);
+      if(tmp) return tmp;
+      break;
+    } else return iter;
+  }
+  return iter;
 }
 
 TreeNode _ceiling(TreeNode root, int key) {
-	TreeNode iter = root, tmp = NULL;
-	while(iter) {
-		if(key > iter->key) iter = iter->right;
-		else if(key < iter->key) {
-			tmp = _ceiling(root->left, key);
-			if(tmp) return tmp;
-			break;
-		} else return iter;
-	}
-	return iter;
+  TreeNode iter = root, tmp = NULL;
+  while(iter) {
+    if(key > iter->key) iter = iter->right;
+    else if(key < iter->key) {
+      tmp = _ceiling(root->left, key);
+      if(tmp) return tmp;
+      break;
+    } else return iter;
+  }
+  return iter;
 }
 
 TreeNode getFloor(BinarySearchTree bst, int key) {
-	return _floor(bst->root, key);
+  return _floor(bst->root, key);
 }
 
 TreeNode getCeiling(BinarySearchTree bst, int key) {
-	return _ceiling(bst->root, key);
+  return _ceiling(bst->root, key);
 }
 
 void removeBinarySearchTree(BinarySearchTree bst) {
-	removeTreeNode(bst->root);
-	bst = NULL;
+  removeTreeNode(bst->root);
+  bst = NULL;
 }
 
 void _printInorder(TreeNode tn) {
-	if(!tn) return;
-	_printInorder(tn->left);
-	printf("(%d, %d) ", tn->key, tn->val);
-	_printInorder(tn->right);
+  if(!tn) return;
+  _printInorder(tn->left);
+  printf("(%d, %d) ", tn->key, tn->val);
+  _printInorder(tn->right);
 }
 
 void printInorder(BinarySearchTree bst) {
-	_printInorder(bst->root);
-	printf("\n");
+  _printInorder(bst->root);
+  printf("\n");
 }
 
 void testBinarySearchTree() {
-	BinarySearchTree bst = newBinarySearchTree();
-	put(bst, 3, 1);
-	put(bst, 2, 4);
-	put(bst, 6, 3);
-	put(bst, 1, 7);
-	put(bst, 2, 5);
-	put(bst, 8, 4);
-	put(bst, 4, 8);
-	put(bst, 7, 6);
-	put(bst, 9, 3);
-	printf("<BinarySearchTree>\n");
-	printf("size: %d\n", bst->size);
-	printInorder(bst);
-	TreeNode max = getMax(bst);
-	TreeNode min = getMin(bst);
-	TreeNode f = getFloor(bst, 5);
-	TreeNode c = getCeiling(bst, 5);
-	TreeNode s = search(bst, 9);
-	printf("max: (%d, %d)\n", max->key, max->val);
-	printf("min: (%d, %d)\n", min->key, min->val);
-	printf("floor(5): (%d, %d)\n", f->key, f->val);
-	printf("ceiling(5): (%d, %d)\n", c->key, c->val);
-	printf("search(9): (%d, %d)\n", s->key, s->val);
-	printf("after rmv(8)\n");
-	free(rmv(bst, 8));
-	printInorder(bst);
-	removeBinarySearchTree(bst);
+  BinarySearchTree bst = newBinarySearchTree();
+  put(bst, 3, 1);
+  put(bst, 2, 4);
+  put(bst, 6, 3);
+  put(bst, 1, 7);
+  put(bst, 2, 5);
+  put(bst, 8, 4);
+  put(bst, 4, 8);
+  put(bst, 7, 6);
+  put(bst, 9, 3);
+  printf("<BinarySearchTree>\n");
+  printf("size: %d\n", bst->size);
+  printInorder(bst);
+  TreeNode max = getMax(bst);
+  TreeNode min = getMin(bst);
+  TreeNode f = getFloor(bst, 5);
+  TreeNode c = getCeiling(bst, 5);
+  TreeNode s = search(bst, 9);
+  printf("max: (%d, %d)\n", max->key, max->val);
+  printf("min: (%d, %d)\n", min->key, min->val);
+  printf("floor(5): (%d, %d)\n", f->key, f->val);
+  printf("ceiling(5): (%d, %d)\n", c->key, c->val);
+  printf("search(9): (%d, %d)\n", s->key, s->val);
+  printf("after rmv(8)\n");
+  free(rmv(bst, 8));
+  printInorder(bst);
+  removeBinarySearchTree(bst);
 }
 
 int main() {
-	testBinarySearchTree();
-	return 0;
+  testBinarySearchTree();
+  return 0;
 }
--- c/LinkedList.c
+++ c/LinkedList.c
@@ -4,21 +4,21 @@
 
 typedef struct _node* Node;
 typedef struct _node {
-	int data;
-	Node next;
+  int data;
+  Node next;
 } node;
 
 typedef struct _linkedList {
-	Node head;
-	int size;
+  Node head;
+  int size;
 } linkedList;
 typedef linkedList* LinkedList;
 
 LinkedList newLinkedList() {
-	LinkedList ret = (LinkedList)malloc(sizeof(linkedList));
-	ret->head = NULL;
-	ret->size = 0;
-	return ret;
+  LinkedList ret = (LinkedList)malloc(sizeof(linkedList));
+  ret->head = NULL;
+  ret->size = 0;
+  return ret;
 }
 
 Node newNode(int data) {
@@ -29,99 +29,99 @@ Node newNode(int data) {
 }
 
 void add(LinkedList ll, int data) {
-	if(!ll->head) ll->head = newNode(data);
-	else {
-		Node iter = ll->head;
-		while(iter->next) iter = iter->next;
-		iter->next = newNode(data);
-	}
-	ll->size++;
+  if(!ll->head) ll->head = newNode(data);
+  else {
+    Node iter = ll->head;
+    while(iter->next) iter = iter->next;
+    iter->next = newNode(data);
+  }
+  ll->size++;
 }
 
 Node rmv(LinkedList ll, int data) {
-	if(!ll->head) return NULL;
-	else if(ll->head->data == data) {
-		Node ret = ll->head;
-		ll->head = ret->next;
-		ll->size--;
-		return ret;
-	}
-	else {
-		Node ret = NULL, iter = ll->head;
-		while(iter->next) {
-			if(iter->next->data == data) {
-				ret = iter->next;
-				iter->next = ret->next;
-				ll->size--;
-				break;
-			}
-			iter = iter->next;
-		}
-		return ret;
-	}
+  if(!ll->head) return NULL;
+  else if(ll->head->data == data) {
+    Node ret = ll->head;
+    ll->head = ret->next;
+    ll->size--;
+    return ret;
+  }
+  else {
+    Node ret = NULL, iter = ll->head;
+    while(iter->next) {
+      if(iter->next->data == data) {
+        ret = iter->next;
+        iter->next = ret->next;
+        ll->size--;
+        break;
+      }
+      iter = iter->next;
+    }
+    return ret;
+  }
 }
 
 Node search(LinkedList ll, int data) {
-	if(!ll->head) return NULL;
-	else {
-		Node ret = NULL, iter = ll->head;
-		while(iter) {
-			if(iter->data == data) {
-				ret = iter;
-				break;
-			}
-			iter = iter->next;
-		}
-		return ret;
-	}
+  if(!ll->head) return NULL;
+  else {
+    Node ret = NULL, iter = ll->head;
+    while(iter) {
+      if(iter->data == data) {
+        ret = iter;
+        break;
+      }
+      iter = iter->next;
+    }
+    return ret;
+  }
 }
 
 void removeLinkedList(LinkedList ll) {
-	Node tmp = NULL, iter = ll->head;
-	while(iter) {
-		tmp = iter;
-		iter = iter->next;
-		free(tmp);
-	}
+  Node tmp = NULL, iter = ll->head;
+  while(iter) {
+    tmp = iter;
+    iter = iter->next;
+    free(tmp);
+  }
 }
 
 void printLinkedList(LinkedList ll) {
-	printf("size: %d\n", ll->size);
-	printf("[ ");
-	Node iter = ll->head;
-	while(iter) {
-		printf("%d ", iter->data);
-		iter = iter->next;
-	}
-	printf("]\n");
+  printf("size: %d\n", ll->size);
+  printf("[ ");
+  Node iter = ll->head;
+  while(iter) {
+    printf("%d ", iter->data);
+    iter = iter->next;
+  }
+  printf("]\n");
 }
 
 void testLinkedList() {
-	LinkedList ll = newLinkedList();
+  LinkedList ll = newLinkedList();
 
-	add(ll, 3);
-	add(ll, 2);
-	add(ll, 4);
-	add(ll, 8);
-	add(ll, 7);
-	
-	printLinkedList(ll);
+  add(ll, 3);
+  add(ll, 2);
+  add(ll, 4);
+  add(ll, 8);
+  add(ll, 7);
+  
+  printLinkedList(ll);
 
-	printf("rmv(%d)\n", 4);
-	rmv(ll, 4);
+  printf("rmv(%d)\n", 4);
+  rmv(ll, 4);
 
-	printLinkedList(ll);
+  printLinkedList(ll);
 
-	printf("search(%d): %d\n", 3, search(ll, 3)->data);
-	Node n = search(ll, 4);
-	printf("search(%d): ", 4);
-	if(n) printf("%d\n", n->data);
-	else printf("couldn't find %d\n", 4);
+  printf("search(%d): %d\n", 3, search(ll, 3)->data);
+  Node n = search(ll, 4);
+  printf("search(%d): ", 4);
+  if(n) printf("%d\n", n->data);
+  else printf("couldn't find %d\n", 4);
 
-	removeLinkedList(ll);
+  removeLinkedList(ll);
 }
 
 int main() {
-	testLinkedList();
-	return 0;
+  testLinkedList();
+  return 0;
 }
