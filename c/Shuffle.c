451158d548bb9f3559c55a1c01270fd203309e42
arkainoh <arkainoh@gmail.com>
2018-06-02 20:46:03 +0900

    fix indentation

--- c/Shuffle.c
+++ c/Shuffle.c
@@ -3,27 +3,27 @@
 #include <stdlib.h>
 
 void swap(int* a, int* b) {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
 }
 
 void Shuffle(int* a, int N) {
-	for(int i = N - 1; i > 0; i--) {
-		int r = rand() % (i + 1);
-		swap(&a[r], &a[i]);
-	}
+  for(int i = N - 1; i > 0; i--) {
+    int r = rand() % (i + 1);
+    swap(&a[r], &a[i]);
+  }
 }
 
 void testShuffle() {
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	Shuffle(a, 10);
-	for(int i = 0; i < 10; i++) printf("%d ", a[i]);
-	printf("\n");
+  int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  Shuffle(a, 10);
+  for(int i = 0; i < 10; i++) printf("%d ", a[i]);
+  printf("\n");
 }
 
 int main() {
-	srand(time(NULL));
-	testShuffle();
-	return 0;
+  srand(time(NULL));
+  testShuffle();
+  return 0;
 }
