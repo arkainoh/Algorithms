cc108182588adb759c88ef157b5e956024f8d62a
arkainoh <arkainoh@gmail.com>
2018-06-23 14:44:03 +0900

    modify swap() for consistency

--- c/Shuffle.c
+++ c/Shuffle.c
@@ -2,23 +2,23 @@
 #include <time.h>
 #include <stdlib.h>
 
-void swap(int* a, int* b) {
-  int tmp = *a;
-  *a = *b;
-  *b = tmp;
+void swap(int* arr, int i, int j) {
+  int tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
 }
 
-void Shuffle(int* a, int N) {
+void Shuffle(int* arr, int N) {
   for(int i = N - 1; i > 0; i--) {
     int r = rand() % (i + 1);
-    swap(&a[r], &a[i]);
+    swap(arr, r, i);
   }
 }
 
 void testShuffle() {
-  int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  Shuffle(a, 10);
-  for(int i = 0; i < 10; i++) printf("%d ", a[i]);
+  int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  Shuffle(arr, 10);
+  for(int i = 0; i < 10; i++) printf("%d ", arr[i]);
   printf("\n");
 }
 
